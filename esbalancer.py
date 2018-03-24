6553bf2f83219b270ec976fbfc10510b2a3d12cf
starcatter <30572485+starcatter@users.noreply.github.com>
2018-03-24 01:46:07 +0100

    Added ship attribute modification support
    Added _self-> prefix for attribute values that are not to include linked outfits/submunitions
    Added ability to output subattributes eg. weapon->hull damage
    Added formatting options, precision (decimal places) and multiple (output value is multiple of entered value)
    Added lessThan filter to criteria section
    Added usage examples to balancer.json

--- esbalancer.py
+++ esbalancer.py
@@ -19,15 +19,28 @@ outfitFileContents = {}
 
 # ----------------------------------------------------
 
+def is_number(s):
+    try:
+        float(s)
+        return True
+    except ValueError:
+        return False
+
 def quoteName(string : str) -> str:
-	if string.count('"') > 0:
+	if string is None:
+		return ''
+	elif string.count('"') > 0:
 		return '`' + string + '`'
 	elif string.count(' ') > 0:
 		return '"' + string + '"'
 	return string
 	
 def quoteValue(string : str) -> str:
-	if string.count('"') > 0:
+	if string is None:
+		return ''
+	elif not isinstance(string,str):
+		return str(string)
+	elif string.count('"') > 0:
 		return '`' + string + '`'
 	elif string.isalpha() or string.count(' ') > 0:
 		return '"' + string + '"'
@@ -36,14 +49,109 @@ def quoteValue(string : str) -> str:
 # ----------------------------------------------
 
 class Attribute:
+
+	def __init__(self, name:str, values:[str]):
+		self.name = name.replace("\n","")
+
+		if(values != None): 
+			self.values = [value for value in values]
+		else:
+			self.values = None
+
+		self.attributes = {}	# sub-attrs
+		self.linked = []		# entities linked to this attr (root)
+
+	# ---
+	# Handle linked entities
+	# ---
+
 	def addLink(self, entity, count):
 		self.linked.append((entity,count))
 
+	# ---
+	# Handle sub-attributes
+	# ---
+	def getAttachedValue(self, key) -> float:
+		if (len(self.linked) == 0): return None
+
+		value = 0.0
+
+		for link in self.linked:
+			linkedEntity = link[0]
+			if len(link) > 1:
+				linkedCount = link[1]
+			else:
+				linkedCount = 1
+					
+			linkedAttrVal = linkedEntity.getFirstAttrValue(key)
+			if linkedAttrVal != None:
+				value += float(linkedAttrVal) * float(linkedCount)
+		
+		return value
+
+
+	def getFirstAttrValue(self, key:str, includeAttached:bool = True) -> str:
+		if( key.count("_self->") > 0 ):
+			queryParts  : [str] = key.split("->",1)
+			return(self.getFirstAttrValue(queryParts[1],False))
+
+		# print("get [["+self.name+"]]::[["+key+"]]")
+
+		attrList = self.getPath(key,False)
+		if attrList is None:
+			if includeAttached:
+				return self.getAttachedValue(key)
+			else:
+				return None
+
+		firstValue = attrList[0].values[0]
+
+		if includeAttached == False or len(self.linked) == 0:
+			return firstValue
+
+		if is_number(firstValue):
+			attachedValue = self.getAttachedValue(key)
+			if attachedValue is not None:
+				value = float(firstValue) + attachedValue 
+				return str(value)
+		
+		return firstValue
+
+	def getAttrList(self, key:str, create : bool = False) -> ['Attribute']:
+		if key in self.attributes:
+			return self.attributes[key]
+		elif create:
+			self.attributes[key] = [Attribute(key,None)]
+			return self.attributes[key]
+		else:
+			# print("self.getAttrList [["+key+"]] -> None; \n\t" + str( self.attributes.keys() ))
+			return None
+
+	def getPath(self, key:str, create:bool = False) -> ['Attribute']:
+		if( key.count("->") > 0 ):
+			queryParts  : [str] = key.split("->",1)
+		
+			outerName : str = queryParts[0]
+			innerName : str = queryParts[1]
+
+			outerAttrs = self.getAttrList(outerName,create)
+			if outerAttrs is not None:
+				for candidate in outerAttrs: # return the first attribute in the list that has the innerName attr we need
+					result = candidate.getPath(innerName, create)
+					if result != None: return result
+						
+			return None
+		else:
+			return self.getAttrList(key,create)
+		
 	def addAttr(self, attr:'Attribute'):
 		if(attr.name not in self.attributes):
 			self.attributes[attr.name] = [attr]
 		else:
 			self.attributes[attr.name].append(attr)
+	# ---
+	# File output formatting
+	# ---
 
 	def getString(self,indent = 0) -> str:
 		indentStr = ""
@@ -69,17 +177,6 @@ class Attribute:
 				for attr in attrList:
 					attrStr += attr.getString(indent)
 		return attrStr
-
-	def __init__(self, name:str, values:[str]):
-		self.name = name.replace("\n","")
-
-		if(values != None): 
-			self.values = [value for value in values]
-		else:
-			self.values = None
-
-		self.attributes = {}
-		self.linked = []
 
 class Entity(Attribute):
 	def __init__(self, name:str, entityName : str):
@@ -212,8 +309,13 @@ def loadFile(fileName) -> {}:
 # ----------------------------------------------
 def saveContents():
 	print("Save:")
-	for fileName in setupData["Files"]["Outfits"]:
-		print(fileName)
+	for typeName in setupData["Files"].keys():
+		saveEntities(typeName)
+		
+def saveEntities(entityType : str):
+	print("\t"+entityType+":")
+	for fileName in setupData["Files"][entityType]:
+		print("\t\t"+fileName)
 
 		contents = outfitFileContents[fileName]
 
@@ -224,28 +326,8 @@ def saveContents():
 				else:				
 					print( entry.getString(), file=fileHandle )
 					print( "", file=fileHandle )
-						
-# ----------------------------------------------------
-def getKeyFromAttribute(key:str,attr:Attribute) -> [Attribute]:
-	if( key.count("->") > 0 ):
-		queryParts  : [str] = key.split("->",1)
-		
-		outerName : str = queryParts[0]
-		innerName : str = queryParts[1]
 
-		if outerName in attr.attributes:
-			attrList : [Attribute] = attr.attributes[outerName]
-			for candidate in attrList: # return the first attribute in the list that has the innerName attr we need
-				result = getKeyFromAttribute(queryParts[1], candidate)
-				if result != None: return result
-						
-		return None
-	else:
-		if key in attr.attributes:
-			return attr.attributes[key]
-		else:
-			return None
-
+					
 # ----------------------------------------------		
 def calculateValuesForOutfit(outfit : Outfit, stepCalculations:{} ) -> {str,float}:
 	values : {str,float} = {}
@@ -254,43 +336,34 @@ def calculateValuesForOutfit(outfit : Outfit, stepCalculations:{} ) -> {str,floa
 		params : {str,float} = {}
 
 		# get required params - skip outfit on fail
-		for param, source in process["withRequired"].items():
-			attrList = getKeyFromAttribute(source,outfit)
-			if attrList != None:
-				params[param] = float(attrList[0].values[0])
-			else:
-				return None
+		if "withRequired" in process:
+			for param, source in process["withRequired"].items():
+				attrValue = outfit.getFirstAttrValue(source)
+				if attrValue != None:
+					params[param] = float(attrValue)
+				else:
+					return None
 
 		# get optional data - default to zero on fail
-		for param, source in process["with"].items():
-			attrList = getKeyFromAttribute(source,outfit)
-			if attrList != None:
-				params[param] = float(attrList[0].values[0])
-			else:
-				params[param] = 0.0
-
-		if len(outfit.linked) > 0:
-			for link in outfit.linked:
-				linkedEntity = link[0]
-				if len(link) > 1:
-					linkedCount = link[1]
+		if "with" in process:
+			for param, source in process["with"].items():
+				attrValue = outfit.getFirstAttrValue(source)
+				if attrValue != None:
+					params[param] = float(attrValue)
 				else:
-					linkedCount = 1
+					params[param] = 0.0
 
-				for param, source in process["withRequired"].items():
-					attrList = getKeyFromAttribute(source,linkedEntity)
-					if attrList != None:
-						params[param] += float(attrList[0].values[0])*float(linkedCount)
-				for param, source in process["with"].items():
-					attrList = getKeyFromAttribute(source,linkedEntity)
-					if attrList != None:
-						params[param] += float(attrList[0].values[0])*float(linkedCount)
-
+		# print("\t |-> "+value+" :: "+process["formula"]+": " + str(params))
 		values[value] = parser.parse(process["formula"]).evaluate(params)
 				
 	return values
 
 # ----------------------------------------------
+def filter_greaterThan(attrValue:str,condData) -> bool:
+	floatValue = float(attrValue)
+	floatCond = float(condData)
+	return floatValue > floatCond
+
 def filter_containsAny(attrValue:str,condData) -> bool:
 	for value in condData:
 		if attrValue.lower().count( value.lower() ) > 0: 
@@ -298,10 +371,11 @@ def filter_containsAny(attrValue:str,condData) -> bool:
 	return False
 
 filters = {
-	"containsAny": filter_containsAny 
+	"containsAny": filter_containsAny,
+	"greaterThan": filter_greaterThan,
 	}
 
-def checkCriteriaForOutfit(outfit:Outfit, criteria:{}) -> bool:
+def checkCriteriaForOutfit(outfit:Entity, criteria:{}) -> bool:
 	for attrName,condition in criteria.items():
 		if attrName == "_name":
 			if isinstance(condition, str):
@@ -310,85 +384,120 @@ def checkCriteriaForOutfit(outfit:Outfit, criteria:{}) -> bool:
 				for condType, condData in condition.items():
 					if filters[condType](outfit.name,condData) == False: return False
 		else:
-			attrList : [Attribute] = getKeyFromAttribute(attrName,outfit)
-			if attrList != None:
-				for attr in attrList:
-					if isinstance(condition, str):
-						if attr.values[0] != condition: return False
-					else:
-						for condType, condData in condition.items():
-							if filters[condType](attr.values[0],condData) == False: return False
-			else:
+			attrValue = outfit.getFirstAttrValue(attrName)
+
+			if attrValue == None: 
 				return False
+
+			if isinstance(condition, str):
+				if attrValue != condition: return False
+			else:
+				for condType, condData in condition.items():
+					if filters[condType](attrValue,condData) == False: return False
 
 	return True
 
 # ----------------------------------------------
 def execStep(setup : {}):
 	stepName :str = setup["name"]
+	stepTarget :str = setup["type"]
 	stepCriteria : {str,str} = setup["criteria"]
-	stepReference : [str] = setup["reference"]
-	stepCalculations : {} = setup["calculate"]
+	stepReference : [str] = setup["reference"] if ("reference" in setup) else  None
+	stepCalculations : {} = setup["calculate"] if ("calculate" in setup) else  None
 	stepActions : {} = setup["set"]
 
-	print("Execute step [["+stepName+"]]")
+	referenceValues = {}
+
+	print("")
+	print("Execute step [["+stepName+"]] targeting [["+stepTarget+"]]")
 
 	reference : [Outfit] = []
 	workSet : [Outfit] = []
 
-	for index,outfit in entityIndex["outfit"].items():
+	for index,outfit in entityIndex[stepTarget].items():
 		if checkCriteriaForOutfit(outfit, stepCriteria):
 			workSet.append(outfit)
 
-	for refName in stepReference:
-		if refName in entityIndex["outfit"]:
-			reference.append(entityIndex["outfit"][refName])
-
+	print( "\t processing "+ str(len(workSet)) + " "+ stepTarget +"s")
+		
 	# ---
-	print( "\t processing "+ str(len(workSet)) + " outfits")
-	
-	referenceValues = {}
-	for outfit in reference:
-		print( "\t reference item: "+ outfit.name)
-		referenceValues[outfit.name] = calculateValuesForOutfit(outfit, stepCalculations)
-		for key,value in referenceValues[outfit.name].items():
-			print( "\t\t - "+ key +": "+str(value))
+	# get reference entities
+	# ---
+	if stepReference is not None:
+		for refName in stepReference:
+			if refName in entityIndex[stepTarget]:
+				reference.append(entityIndex[stepTarget][refName])
 
-	print("")
+		for outfit in reference:
+			print( "\t reference item: "+ outfit.name)
+			referenceValues[outfit.name] = calculateValuesForOutfit(outfit, stepCalculations)
+			for key,value in referenceValues[outfit.name].items():
+				print( "\t\t - "+ key +": "+str(value))
+
+		print("")
 
 	for outfit in workSet:
 		
 		print("\n\t [ "+outfit.name+" ]")
-		values = calculateValuesForOutfit(outfit, stepCalculations)
-		if values == None:
-			print("\t |-> Skip outfit")
-			continue
+		if stepCalculations is not None:
+			values = calculateValuesForOutfit(outfit, stepCalculations)
+			if values == None:
+				print("\t |-> Skip " + stepTarget + " @ calculateValuesForOutfit()")
+				continue
+		else:
+			values = {}
 
 		for attributeName, process in stepActions.items():
 			params : {str,float} = {}
 
-			for param, source in process["with"].items():
-				attrList = getKeyFromAttribute(source,outfit)
-				if attrList != None:
-					params[param] = float(attrList[0].values[0])
-				else:
-					params[param] = 0.0
+			if "with" in process:
+				for param, source in process["with"].items():
+					attrValue = outfit.getFirstAttrValue(source)
+
+					if attrValue != None:
+						params[param] = float(attrValue)
+					else:
+						# TODO: skip here
+						print("\t |-> Missing " + param + " @ set->with")
+						params[param] = 0.0						
 
 			for valueName, value in values.items():
 				params[valueName] = value
 
-			#TODO: pick closest ref item
-			for paramName, paramValue in referenceValues[ reference[0].name ].items():
-				params["reference."+paramName] = paramValue
+			if stepReference is not None:
+				#TODO: pick closest ref item
+				for paramName, paramValue in referenceValues[ reference[0].name ].items():
+					params["reference."+paramName] = paramValue
+
+			# ----------------------------------------------		
+			# calculate final value
+			# ----------------------------------------------	
+
+			print("\t |-> "+process["formula"]+": " + str(params))
 
 			floatValue : float = parser.parse(process["formula"]).evaluate(params)
-			value = str.format("{:.1f}",floatValue);
-			
+			value = floatValue
+
+			# ----------------------------------------------		
+			# handle value format
+			# ----------------------------------------------		
+
+			if( "format" in process ):
+				formatDef : {} = process["format"]
+				if( "precision" in formatDef ):
+					formatPrecision = formatDef["precision"]
+					formatStr = "{:."+formatPrecision+"f}"
+					value = str.format(formatStr,floatValue)
+				elif( "multiple" in formatDef ):
+					formatMultiple : float = float(formatDef["multiple"])
+					value = floatValue - (floatValue % formatMultiple)					
+
 			# ----------------------------------------------		
 			# set the value
 			# ----------------------------------------------		
-			outfit.attributes[attributeName] = [Attribute(attributeName,[value])]
-			print("\t |-> "+attributeName+": " + value)
+			attrList = outfit.getPath(attributeName,True)
+			attrList[0].values = [value]
+			print("\t |-> "+attributeName+": " + str(value))
 
 # ----------------------------------------------			
 def alterContents():
@@ -412,7 +521,7 @@ def assembleContents():
 				
 				linkClean,linkIndex = parseLink(link)
 
-				attrList : Attribute = getKeyFromAttribute(linkClean, entity)
+				attrList : [Attribute] = entity.getPath(linkClean)
 				if attrList != None:
 					for ref in attrList:
 						if len(ref.attributes) == 0 and len(ref.values) > linkIndex:
